14b87930fae45ea988f3da5531be53c5f56a75ae
root <root@ubuntu.(none)>
2015-09-05 21:52:30 +0800

    change encrypt

--- des3.py
+++ des3.py
@@ -4,11 +4,7 @@ import base64
 import des
 
 class des3:
-   #des1 = None
 
-   #def __init(self):
-   #    k = self.getKey()
-   #    des1 = des(k,CBC,"\0\0\0\0\0\0\0\0",pad=None,padmode=PAD_PKCS5)
     
 
    def getKey(self,key):
@@ -19,28 +15,28 @@ class des3:
        return m1.hexdigest()
 
    def encrypt(self,json,key):
-       k = self.getKey(key)
+       k = self.getKey(key)[0:24]
        #k = k[0:8]
-       #des1 = des(k,CBC,"\0\0\0\0\0\0\0\0",pad=None,padmode=PAD_PKCS5)
-       #tmp = des1.encrypt(json)
-       des1 = des.DES()
-       des1.input_key(k)
-       tmp = des1.encode(json)
+       des1 = triple_des(k,CBC,"abcdefgh",pad=None,padmode=PAD_PKCS5)
+       tmp = des1.encrypt(json)
+       #des1 = des.DES()
+       #des1.input_key(k)
+       #tmp = des1.encode(json)
        res = base64.encodestring(tmp)
        return res
 
    def decrypt(self,data,key):
-       k = self.getKey(key)
-       #des1 = des(k,CBC,"\0\0\0\0\0\0\0\0",pad=None,padmode=PAD_PKCS5)
-       des1 = des.DES()
-       des1.input_key(k)
+       k = self.getKey(key)[0:24]
+       des1 = triple_des(k,CBC,"abcdefgh",pad=None,padmode=PAD_PKCS5)
+       #des1 = des.DES()
+       #des1.input_key(k)
        res = base64.decodestring(data)
-       #return des1.decrypt(res)
-       return des1.decode(res)
+       return des1.decrypt(res)
+       #return des1.decode(res)
 
 if __name__ =="__main__":
    ss = "this is test"
-   key = "abcdefgh"
+   key = "12345678"
    en = des3()
    s = en.encrypt(ss,key)
    print s 
